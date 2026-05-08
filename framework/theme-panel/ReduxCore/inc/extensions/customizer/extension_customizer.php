6744b0bad666c1dd0b2973498a7fb40698e3a6d6
Qasim Khan <qasimkhan@06:3f:9b:0f:d7:34.home>
2026-05-08 10:07:32 -0400

    bug fixed 8

--- framework/theme-panel/ReduxCore/inc/extensions/customizer/extension_customizer.php
+++ framework/theme-panel/ReduxCore/inc/extensions/customizer/extension_customizer.php
@@ -42,6 +42,8 @@
             private static $post_values = array();
             public static $version = "2.0.0";
             private $options = array();
+            public $upload_dir;
+            public $upload_url;
 
             /**
              * Class Constructor. Defines the args for the extions class
@@ -578,7 +580,7 @@
 
             }
 
-            public function add_section( $id, $args = array(), $wp_customize ) {
+            public function add_section( $id, $args, $wp_customize ) {
 
                 if ( is_a( $id, 'WP_Customize_Section' ) ) {
                     $section = $id;
@@ -601,7 +603,7 @@
              * @param WP_Customize_Panel|string $id   Customize Panel object, or Panel ID.
              * @param array                     $args Optional. Panel arguments. Default empty array.
              */
-            public function add_panel( $id, $args = array(), $wp_customize ) {
+            public function add_panel( $id, $args, $wp_customize ) {
                 if ( is_a( $id, 'WP_Customize_Panel' ) ) {
                     $panel = $id;
                 } else {
