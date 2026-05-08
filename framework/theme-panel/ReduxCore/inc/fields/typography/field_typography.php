281852d69f951c143727b61b9ba192429d3e33ee
Qasim Khan <qasimkhan@06:3f:9b:0f:d7:34.home>
2026-05-08 10:00:06 -0400

    bug fixed deprected

--- framework/theme-panel/ReduxCore/inc/fields/typography/field_typography.php
+++ framework/theme-panel/ReduxCore/inc/fields/typography/field_typography.php
@@ -41,13 +41,17 @@ if ( ! class_exists( 'ReduxFramework_typography' ) ) {
 
         private $user_fonts = true;
 
+        public $parent;
+        public $field;
+        public $value;
+
         /**
          * Field Constructor.
          * Required - must call the parent constructor, then assign field and value to vars, and obviously call the render field function
          *
          * @since ReduxFramework 1.0.0
          */
-        function __construct( $field = array(), $value = '', $parent ) {
+        function __construct( $field, $value, $parent ) {
             $this->parent = $parent;
             $this->field  = $field;
             $this->value  = $value;
